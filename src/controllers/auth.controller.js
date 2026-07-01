fddd7fd25dec5237cdf6da4af455cb5fb9fb7e90
Vikash-Sharma-45 <vikashsharmavk58@gmail.com>
2026-07-01 11:40:23 +0530

    Get-Me Route Create

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -119,8 +119,29 @@ async function logoutUserController(req, res) {
     })
 }
 
+/**
+ * - @name getMeController
+ * - @description get the current logged in user detail.
+ * - @access Private
+ */
+
+async function getMeController(req, res) {
+
+    const user = await userModel.findById(req.user._id)
+
+    res.status(200).json({
+        message : "User details Fetched successfully",
+        user : {
+            id : user._id,
+            username : user.username,
+            email : user.email
+        }
+    })
+}
+
 module.exports = {
     registerUserController,
     loginUserController,
-    logoutUserController
+    logoutUserController,
+    getMeController
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const {Router} = require("express")
 const authController = require("../controllers/auth.controller")
+const authMiddleware = require("../middlewares/auth.middleware")
 
 const authRouter = Router()
 
@@ -27,6 +28,14 @@ authRouter.post("/login", authController.loginUserController)
  */
 
 authRouter.get("/logout", authController.logoutUserController)
+
+/**
+ * - @routes GET /api/auth/get-me
+ * - @description get the current logged in user
+ * - @access Private
+ */
+
+authRouter.get("/get-me", authMiddleware.authUser, authController.getMeController)
 
 
 module.exports = authRouter
