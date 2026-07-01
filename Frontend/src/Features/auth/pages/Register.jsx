acc980f7a1227dac103fe891ffdf5d54cad19d1b
Vikash-Sharma-45 <vikashsharmavk58@gmail.com>
2026-07-01 23:21:09 +0530

    Authentication Form Styling and Architecture Overview

---
+++ Frontend/src/Features/auth/pages/Register.jsx
@@ -0,0 +1,38 @@
+import React from "react"
+import { useNavigate, Link } from "react-router"
+
+const Register = () => {
+
+    const navigate = useNavigate()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+    return(
+        <main>
+            <div className="form-container">
+                <h1>Register</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="input-group">
+                        <label htmlFor="username">Username</label>
+                        <input type="text" id="username" name="username" placeholder="Enter Username" />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" name="email" placeholder="Enter Email Address" />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" name="password" placeholder="Enter Your Password" />
+                    </div>
+
+                    <button class="button primary-button">Register</button>
+                </form>
+
+                <p>Already have an account ? <Link to={"/login"}>Link</Link></p>
+            </div>
+        </main>
+    )
+}
+
+export default Register
